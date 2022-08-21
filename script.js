41f7e9603021d5f051c9f9186be0bb363de81ad1
Snack-Time <wmcnally1226@gmail.com>
2022-08-21 17:05:27 -0400

    Add still wip game function

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
+let playerSelection = '';
+let computerSelection = '';
+let computerWins = 0;
+let playerWins = 0;
+
 function getComputerChoice() {
-    let cpuChoice = Math.floor(Math.random()*3) + 1;
-    if (cpuChoice === 1) {
-        computerSelection = "rock";
+    let cpuRandomChoice = Math.floor(Math.random()*3) + 1;
+    if (cpuRandomChoice === 1) {
+        cpuRandomChoice = "rock";
     }
-    else if (cpuChoice === 2) {
-        computerSelection = "paper";
+    else if (cpuRandomChoice === 2) {
+        cpuRandomChoice = "paper";
     }
-    else if (cpuChoice === 3) {
-        computerSelection = "scissors";
+    else if (cpuRandomChoice === 3) {
+        cpuRandomChoice = "scissors";
     }
     else {
-        computerSelection = "gun";
+        cpuRandomChoice = "gun";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) { 
-     console.log('You drew! No one wins!')
+    console.log('You drew! No one wins!')
     }
 
     else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
-    
-    console.log("LOSER! YOU'RE A LOSER! ARE YOU FEELING SORRY FOR YOURSELF?") }
+    console.log(`Your ${playerSelection} loses to the computer's ${computerSelection}. LOSER! YOU'RE A LOSER! ARE YOU FEELING SORRY FOR YOURSELF?`)
+    computerWins = ++computerWins
+    return computerWins;
+    }
 
     else if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "rock" && computerSelection === "scissors")) {
-    console.log("You're winner!") }
-    
+    console.log(`Your ${playerSelection} beats the computer's ${computerSelection}. You're winner!`)
+    playerWins = ++playerWins
+    return playerWins;
+    }
+}
+
+function game() {
+    playerSelection = prompt("Rock, paper, or scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    playRound(playerSelection, computerSelection);
 }
